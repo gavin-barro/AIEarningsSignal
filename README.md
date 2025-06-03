149d2f14305c31fc0c56bace9b4364662b224dd1
Gavin Barro <g.ab28320@gmail.com>
2025-06-03 14:10:01 -0400

    almost done with step 1

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # AIEarningsSignal
 An AI-powered application that identifies, retrieves, and analyzes the earnings call transcripts for NVIDIA over the last four quarters.
+
+
+https://jsonviewer.stack.hu/
--- src/driver.py
+++ src/driver.py
@@ -1,8 +1,40 @@
+import os
+import requests
+from dotenv import load_dotenv
 
+load_dotenv()
+
+STOCK_API_KEY = os.getenv("STOCK_API_KEY", "")
+STOCK_END_POINT = "https://www.alphavantage.co/query"
+SYMBOL = "NVDA"
+QUARTERS = [
+    {"year": 2025, "quarter": "Q1"},  # Q1 2026 (ended April 2025)
+    {"year": 2025, "quarter": "Q4"},  # Q4 2025 (ended January 2025)
+    {"year": 2024, "quarter": "Q3"},  # Q3 2025 (ended October 2024)
+    {"year": 2024, "quarter": "Q2"},  # Q2 2025 (ended July 2024)
+]
+
+def get_data(year: int, quarter: str):
+    nvda_params = {
+        'function': 'EARNINGS_CALL_TRANSCRIPT',
+        'symbol': SYMBOL,
+        'quarter': f"{year}{quarter}",
+        'apikey': STOCK_API_KEY,
+    }
+    response = requests.get(STOCK_END_POINT, nvda_params)
+    response.raise_for_status()
+    data = response.json()
+    print(data)
 
 def main() -> None:
     #Step 1: Data collection
-    
+    data = []
+    for q in QUARTERS:
+        year = q["year"]
+        quarter = q["quarter"]
+        # data.append(get_data(year, quarter))
+        print(get_data(year, quarter))
+        break
     
     #Step 2: Signal extraction
     
@@ -11,8 +43,6 @@ def main() -> None:
     
     
     #Step 4: Deploy
-    
-    pass
 
 if __name__ == "__main__":
     main()
