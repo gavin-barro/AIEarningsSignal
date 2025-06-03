b751acd8780b5d4a3b448c6c0fb37ba5a2ec26bd
Gavin Barro <g.ab28320@gmail.com>
2025-06-03 13:49:40 -0400

    set up project

---
+++ src/driver.py
@@ -0,0 +1,18 @@
+
+
+def main() -> None:
+    #Step 1: Data collection
+    
+    
+    #Step 2: Signal extraction
+    
+    
+    #Step 3: User Interface
+    
+    
+    #Step 4: Deploy
+    
+    pass
+
+if __name__ == "__main__":
+    main()
